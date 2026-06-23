e9d28e95a839bd6dbb5f11019a2bf1151200d571
SwampingTheSwamp <and695842@gmail.com>
2026-06-23 20:53:31 -0300

    Primeiro

--- Projeto.X/Controle de temperatura.c
+++ Projeto.X/Controle de temperatura.c
@@ -3,8 +3,6 @@
 #include "isr.h"
 #define _XTAL_FREQ 20000000
 
-
-
 #pragma config FOSC = HS // Fosc = 20MHz -> Tcy = 200ns
 #pragma config CPUDIV = OSC1_PLL2
 #pragma config WDT    = ON
@@ -12,97 +10,122 @@
 #pragma config PBADEN = OFF
 #pragma config LVP    = OFF
 
-signed char menu=1;
+unsigned char h = 12, m = 0;
+
+void escreve2dig(unsigned char v);
 
 void main() {
-        char atraso;
-        ADCON1 = 0x0F;
-        TRISBbits.TRISB0 = 1;
-        TRISBbits.TRISB1 = 1;
-        TRISBbits.TRISB2 = 1;
-        TRISAbits.TRISA5 = 0;
-        TRISAbits.TRISA3 = 0;
-        TRISCbits.TRISC1 = 0;
-        TRISCbits.TRISC2 = 0;
-        INTCON2bits.nRBPU = 0;
+    char atraso;
+
+    // --- Configuracao de I/O e A/D ---
+    ADCON1 = 0x0F;              // todos os pinos como digitais (ajustar
+                                 // depois ao habilitar os canais do LM35
+                                 // e do potenciometro)
+    TRISBbits.TRISB0 = 1;       // INT0
+    TRISBbits.TRISB1 = 1;       // INT1
+    TRISBbits.TRISB2 = 1;       // INT2
+    TRISAbits.TRISA5 = 0;
+    TRISAbits.TRISA3 = 0;
+    TRISCbits.TRISC1 = 0;
+    TRISCbits.TRISC2 = 0;
+    INTCON2bits.nRBPU = 0;
+    TRISE = 0x00;
+    TRISD = 0x00;
+
+    // --- Configuração PWM CCP1 usando Timer2 ---
+
+    // Timer2 ligado
+    // Prescaler 16
+    T2CON = 0b00000111;
+
+    // Período do PWM
+    // Fpwm = Fosc / (4 * (PR2+1) * prescaler)
+    // PR2 = 249 -> aproximadamente 5kHz
+    PR2 = 249;
 
 
+    // CCP1 em modo PWM
+    CCP1CON = 0b00001100;
 
-        TRISE = 0x00;
-        TRISD = 0x00;
-        TRISCbits.TRISC2 = 0;
-        ADCON1 = 0x0F; 
 
-        // Configuração do timer 1 com prescaler 8
-        T1CON = 0b10110101;
-        TMR1H = 0;
-        TMR1L = 0;
-        // Seleção de Timer1
-        T3CONbits.T3CCP1 = 0;
-        T3CONbits.T3CCP2 = 0;
+    // Duty cycle inicial 0%
+    CCPR1L = 0;
+    CCP1CONbits.DC1B0 = 0;
+    CCP1CONbits.DC1B1 = 0;
 
-        // Configuração do CCP -> modo comparação
-        CCP1CON = 0b00001011;	 // Special event trigger
-        PIR1bits.CCP1IF = 0;	 // zera flag da interrupção CCP1
-        PIE1bits.CCP1IE = 1;	 // habilita interrupção do CCP1
-        IPR1bits.CCP1IP = 1;
-        //CCPR1 = 3125 = 0x0C35
-        CCPR1H = 0x0C;
-        CCPR1L = 0x35;
+    // --- Configuracao de prioridade e interrupcoes globais ---
+    RCONbits.IPEN = 1;          // habilita niveis de prioridade
+    INTCONbits.PEIE = 1;        // habilita interrupcoes de perifericos
 
-        RCONbits.IPEN = 0;		// desabilita prioridade
-        INTCONbits.PEIE = 1;	// habilita interrupções de periféricos	
-        INTCONbits.GIE = 1;		// habilita interrupções global
-    
-        //Configura??o das interrup??es
-        RCONbits.IPEN = 1; // habilita prioridade
+    // INT0 (sempre alta prioridade por hardware)
+    INTCONbits.INT0IE = 1;
+    INTCONbits.INT0IF = 0;
 
-        // Configura??o de INT0
-        INTCONbits.INT0IE = 1; // habilita INT0
-        INTCONbits.INT0IF = 0; // zera flag da INT0
-        // INT0 ? sempre de alta prioridade
-        // Configura??o de INT1
-        INTCON3bits.INT1IE = 1; // habilita INT1
-        INTCON3bits.INT1IF = 0; // zera flag da INT1
-        INTCON3bits.INT1IP = 0; // interrup??o INT1 ? de baixa prioridade
+    // INT1 (baixa prioridade)
+    INTCON3bits.INT1IE = 1;
+    INTCON3bits.INT1IF = 0;
+    INTCON3bits.INT1IP = 0;
 
-            // Configura??o de INT2
-        INTCON3bits.INT2IE = 1; // habilita INT2
-        INTCON3bits.INT2IF = 0; // zera flag da INT2
-        INTCON3bits.INT2IP = 0; // interrup??o INT2 ? de baixa prioridade
+    // INT2 (baixa prioridade)
+    INTCON3bits.INT2IE = 1;
+    INTCON3bits.INT2IF = 0;
+    INTCON3bits.INT2IP = 0;
 
-        INTCONbits.GIEL = 1; // habilita interrup??es de baixa prioridade
-        INTCONbits.GIEH = 1; // habilita interrup??es de alta prioridade
-   
+    INTCONbits.GIEL = 1;        // habilita interrupcoes de baixa prioridade
+    INTCONbits.GIEH = 1;        // habilita interrupcoes de alta prioridade
 
-        OpenXLCD( FOUR_BIT & LINES_5X7 );
-        WriteCmdXLCD(0x01);
-        __delay_ms(16);
+    // --- Inicializacao do LCD ---
+    OpenXLCD( FOUR_BIT & LINES_5X7 );
+    WriteCmdXLCD(0x01);
+    __delay_ms(16);
+    WriteCmdXLCD(0x80);
+    putrsXLCD("Menu: ");
+    escreve2dig(h);
+    putcXLCD(':');
+    escreve2dig(m);
 
-        WriteCmdXLCD(0x80);
-        putrsXLCD("Opcoes do Menu:");
-        WriteCmdXLCD(0x0C);
-
-while (1) {
+    while (1) {
         CLRWDT();
         PORTAbits.RA5 = 0;
         PORTAbits.RA3 = 1;
+
         WriteCmdXLCD(0xC0);
         switch (menu){
             case 1:
-            putrsXLCD("Controle Aberto ");
-            break;
-
+                putrsXLCD("Controle Aberto ");
+                break;
             case 2:
-            putrsXLCD("Controle Fechado");
-            break;
-
+                putrsXLCD("Controle Fechado");
+                break;
             case 3:
-            putrsXLCD("Liga Ventilador ");
-            break;
-            
+                putrsXLCD("Liga Ventilador ");
+                break;
         }
-       for (atraso=0; atraso<50; atraso++)
+
+        // Consome a confirmacao feita via INT0 dentro da ISR.
+        // Aqui e o lugar certo para, no futuro, chamar as funcoes
+        // que de fato iniciam cada modo (malha aberta / fechada / ventilador).
+        if (menu_confirmado != 0){
+            switch (menu_confirmado){
+                case 1:
+                    // TODO: iniciar controle em malha aberta (PWM fixo 75%)
+                    break;
+                case 2:
+                    // TODO: iniciar controle em malha fechada (PI)
+                    break;
+                case 3:
+                    // TODO: iniciar acionamento do ventilador (40% / 5s)
+                    break;
+            }
+            menu_confirmado = 0;
+        }
+
+        for (atraso = 0; atraso < 50; atraso++)
             __delay_ms(10);
     }
+}
+
+void escreve2dig(unsigned char v) {
+    putcXLCD((v / 10) + '0');
+    putcXLCD((v % 10) + '0');
 }
